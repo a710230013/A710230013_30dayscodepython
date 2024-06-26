917d8be59ccbf1c7b6e82eff46d83dc57b461ea9
Irfan Wahyu Ramadhan <a710230013@student.ums.ac.id>
2024-06-26 15:00:08 +0700

    Create Day 20.py
    Day 20

---
+++ Day 20.py
@@ -0,0 +1,13 @@
+def print_all(items):
+    for item in items:
+        print(item)
+
+my_list = [1, 2, 3]
+
+my_tuple = (4, 5, 6)
+
+my_string = "Hello, world!"
+
+print_all(my_list) 
+print_all(my_tuple)
+print_all(my_string) 
